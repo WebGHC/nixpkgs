d68afdc67e23d63b183f687d59d92c54735908cd
Will Fancher <elvishjerricco@gmail.com>
2017-05-10 16:17:49 -0400

    Add WebAssembly architecture

--- lib/systems/parse.nix
+++ lib/systems/parse.nix
@@ -45,6 +45,7 @@ rec {
     x86_64   = { bits = 64; significantByte = littleEndian; family = "x86"; };
     mips64el = { bits = 32; significantByte = littleEndian; family = "mips"; };
     powerpc  = { bits = 32; significantByte = bigEndian;    family = "powerpc"; };
+    wasm32   = { bits = 32; significantByte = littleEndian; family = "wasm"; };
   };
 
   isVendor = isType "vendor";
