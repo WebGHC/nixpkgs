8f0d8a58676d78b10c02ca01844b4af6448d3a5a
Will Fancher <elvishjerricco@gmail.com>
2017-08-29 19:32:39 -0400

    Allow finding revisions in extra remotes in fetchgit
    Necessary for repos like GHC, whose submodules expect origin to be at
    a particular url

--- pkgs/build-support/fetchgit/default.nix
+++ pkgs/build-support/fetchgit/default.nix
@@ -15,6 +15,7 @@ in
 , fetchSubmodules ? true, deepClone ? false
 , branchName ? null
 , name ? urlToName url rev
+, extraRemotes ? []
 , # Shell code executed after the file has been fetched
   # successfully. This can do things like check or transform the file.
   postFetch ? ""
@@ -57,7 +58,7 @@ stdenv.mkDerivation {
   outputHashMode = "recursive";
   outputHash = sha256;
 
-  inherit url rev leaveDotGit fetchSubmodules deepClone branchName postFetch;
+  inherit url rev leaveDotGit fetchSubmodules deepClone branchName postFetch extraRemotes;
 
   GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
